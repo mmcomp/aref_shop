cda788b645db811f524a11ea29917f45dc1d457a
marykhodaparast <maryam.kh1374@gmail.com>
2021-04-05 16:52:29 +0430

    change format of UserResource to be acceptable

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -3,6 +3,8 @@
 namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
+use App\Models\User;
+use App\Models\City;
 
 class UserResource extends JsonResource
 {
@@ -18,16 +20,17 @@ class UserResource extends JsonResource
         if($this->resource != null){
             return [
                 'id' => $this->id,
+                'email' => $this->email,
                 'first_name' => $this->first_name,
                 'last_name' => $this->last_name,
                 'avatar_path' => $this->avatar_path,
-                'referrer_users_id' => $this->referrer_users_id,
+                'referrer_users_id' => User::select('id','first_name','last_name')->where('is_deleted',false)->where('id',$this->referrer_users_id)->first(),
                 'address' => $this->address,
                 'postall' => $this->postall,
-                'cities_id' => ($this->city) ? $this->city->name : '-',
+                'cities_id' => City::select('id','name')->where('is_deleted',false)->where('id',$this->cities_id)->first(),
                 'created_at' => $this->created_at,
                 'updated_at' => $this->updated_at,
-                'groups_id' => ($this->group) ? $this->group->name : '-',
+                'groups_id' => ($this->group) ? $this->group->name : null,
             ];
         }
     }
