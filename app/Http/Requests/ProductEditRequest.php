3a568dfc2b7b55a83345249361f70932eeab7d28
marykhodaparast <maryam.kh1374@gmail.com>
2021-04-10 18:38:55 +0430

    remove unnecessary line

--- app/Http/Requests/ProductEditRequest.php
+++ app/Http/Requests/ProductEditRequest.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Requests;
 
-use App\Rules\IfBetweenTwoFields;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Validation\ValidationException;
 use Illuminate\Http\Exceptions\HttpResponseException;
