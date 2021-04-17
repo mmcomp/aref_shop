219d70dd37411148a1c2684d5d518b7842899ddf
marykhodaparast <maryam.kh1374@gmail.com>
2021-04-17 13:17:15 +0430

    coupons crud is created and tested

---
+++ app/Http/Requests/CouponIndexRequest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\ValidationException;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class CouponIndexRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+     /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'sort' => 'required_with:type|in:id,name,description,amount,type,expired_at,products_id',
+            'type' => 'required_with:sort|in:asc,desc',
+            'per_page' => 'string|max:255'
+        ];
+    }
+     /**
+     * Configure the validator instance.
+     *
+     * @param  \Illuminate\Validation\Validator  $validator
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function withValidator($validator)
+    {
+        if ($validator->fails()) {
+            $errors = (new ValidationException($validator))->errors();
+
+            throw new HttpResponseException(
+                response()->json(['errors' => $errors], 422)
+            );
+        }
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,9 +4,13 @@ namespace Database\Seeders;
 
 use Database\Seeders\GroupGatesSeeder;
 use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
+    protected $toTruncate = ['group_gates'];
+
     /**
      * Seed the application's database.
      *
@@ -14,6 +18,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+
+        Model::unguard();
+
+        foreach($this->toTruncate as $table) {
+            DB::table($table)->truncate();
+        }
         $this->call(GroupGatesSeeder::class);
+
+        Model::reguard();
     }
 }
