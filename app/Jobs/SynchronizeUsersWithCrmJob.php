73333133dfde04f0b4c5c23f26f2e90b37ed1186
marykhodaparast <maryam.kh1374@gmail.com>
2021-04-29 14:13:21 +0430

    first step of creating a queue

---
+++ app/Jobs/SynchronizeUsersWithCrmJob.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Http;
+
+
+class SynchronizeUsersWithCrmJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        //TODO: do the job here
+        $response = Http::post('http://localhost:8000/api/students', [
+           
+        ]);   
+    }
+}
