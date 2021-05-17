c26faba1f6bbd6e79c647221a74768b322521ee4
marykhodaparast <maryam.kh1374@gmail.com>
2021-05-17 12:41:16 +0430

    change seeders

--- database/seeders/GroupGatesSeeder.php
+++ database/seeders/GroupGatesSeeder.php
@@ -16,9 +16,11 @@ class GroupGatesSeeder extends Seeder
     public function run()
     {
 
-        $arr = ['city', 'group', 'product', 'productDetailChair', 'productDetailDownload', 'productDetailPackage', 'productDetailVideo',
-            'province', 'user', 'category-one', 'category-two', 'category-three', 'group_gate','coupon', 'video-session', 'file','product-file','video-session-file'];
-        for ($i = 0; $i < count($arr) ; $i++) {
+        $arr = [
+            'city', 'group', 'product', 'productDetailChair', 'productDetailDownload', 'productDetailPackage', 'productDetailVideo',
+            'province', 'user', 'category-one', 'category-two', 'category-three', 'group_gate', 'coupon', 'video-session', 'file', 'product-file', 'video-session-file'
+        ];
+        for ($i = 0; $i < count($arr); $i++) {
             DB::table('group_gates')->insert([
                 'groups_id' => 1,
                 'users_id' => 1,
@@ -27,19 +29,15 @@ class GroupGatesSeeder extends Seeder
                 'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
             ]);
         }
-        DB::table('group_gates')->insert([
-            'groups_id' => 2,
-            'users_id' => 1,
-            'key' => 'cart',
-            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
-            'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
-        ]);
-        DB::table('group_gates')->insert([
-            'groups_id' => 2,
-            'users_id' => 1,
-            'key' => 'product-of-user',
-            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
-            'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
-        ]);
+        $keys = ['cart', 'product-of-user'];
+        for ($i = 0; $i < 2; $i++) {
+            DB::table('group_gates')->insert([
+                'groups_id' => 2,
+                'users_id' => 1,
+                'key' => $keys[$i],
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+            ]);
+        }
     }
 }
