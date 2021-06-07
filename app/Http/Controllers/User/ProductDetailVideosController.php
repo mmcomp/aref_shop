cd48962c2b860d47196f662253a8c03fcc170605
marykhodaparast <maryam.kh1374@gmail.com>
2021-06-07 15:53:11 +0430

    add api like product_detail_videos_show for user but not completed yet

---
+++ app/Http/Controllers/User/ProductDetailVideosController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers\User;
+
+use App\Http\Controllers\Controller;
+use App\Models\ProductDetailVideo;
+use App\Models\UserVideoSession;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Resources\User\ProductDetailVideosResourceForShow;
+
+class ProductDetailVideosController extends Controller
+{
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function show($id)
+    {
+
+        $product_detail_video = ProductDetailVideo::where('is_deleted', false)->find($id);
+        if ($product_detail_video != null) {
+            $found_user_videoSession = UserVideoSession::where('users_id', Auth::user()->id)->where('video_sessions_id', $product_detail_video->video_sessions_id)->first();
+            $price = $product_detail_video->price != null ? $product_detail_video->price : $product_detail_video->videoSession->price;
+            $checkPriceAndUserVideoSession = (!$price || $found_user_videoSession);
+            return ((new ProductDetailVideosResourceForShow($product_detail_video))->check($checkPriceAndUserVideoSession))->additional([
+                'error' => null,
+            ])->response()->setStatusCode(200);
+        }
+        return (new ProductDetailVideosResourceForShow(null))->additional([
+            'error' => 'ProductDetailVideo not found!',
+        ])->response()->setStatusCode(404);
+    }
+}
---
+++ app/Http/Resources/User/ProductDetailVideosResourceForShow.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Resources\User;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+use App\Http\Resources\FileResource;
+use App\Http\Resources\User\VideoSessionsResource;
+
+class ProductDetailVideosResourceForShow extends JsonResource
+{
+    protected $value;
+
+    public function check($value)
+    {
+        $this->check = $value;
+        return $this;
+    }
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        $files = [];       
+        if ($this->resource != null) {
+            if($this->videoSession){
+                foreach($this->videoSession->videoSessionFiles as $file){
+                    if($file->file != null) {
+                        $files[] = new FileResource($file->file);
+                    }
+                }
+            }
+            return [
+                'id' => $this->id,
+                'name' => $this->name,
+                'product' => $this->product,
+                'price' => $this->price,
+                'files' => $files,
+                'video_session' => (new VideoSessionsResource($this->videoSession))->checkToShowUrlOrNot($this->check),
+                'extraordinary' => $this->extraordinary,
+                'is_hidden' => $this->is_hidden,
+                'single_purchase' => $this->single_purchase,
+                'created_at' => $this->created_at,
+                'updated_at' => $this->updated_at,
+            ];
+        }    }
+}
