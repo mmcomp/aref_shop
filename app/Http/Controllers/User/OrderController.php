17d8fb02cad0aea17eafcb0dcab27b170068dfee
marykhodaparast <maryam.kh1374@gmail.com>
2021-06-20 12:35:43 +0430

    remove if from foreach and instead add query in orderVideoDetails

--- app/Http/Controllers/User/OrderController.php
+++ app/Http/Controllers/User/OrderController.php
@@ -56,7 +56,7 @@ class OrderController extends Controller
             'error' => null,
         ])->response()->setStatusCode(200);
     }
-     /**
+    /**
      * show single sessions of authenticated user
      *
      * @return \Illuminate\Http\JsonResponse
@@ -66,20 +66,24 @@ class OrderController extends Controller
 
         $user_id = Auth::user()->id;
         $orderVideoDetailsArr = [];
-        $orderVideoDetails = OrderVideoDetail::get();
-        foreach($orderVideoDetails as $orderVideoDetail) {
+        $orderVideoDetails = OrderVideoDetail::whereHas('orderDetail', function ($query) {
+            $query->where('all_videos_buy', 0);
+        })->whereHas('orderDetail.order', function ($query) use ($user_id) {
+            $query->where('status', 'ok')->where('users_id', $user_id);
+        })->whereHas('orderDetail.product', function ($query) {
+            $query->where('type', 'video');
+        })->get();
+
+        foreach ($orderVideoDetails as $orderVideoDetail) {
             $product_detail_video = ProductDetailVideo::where('is_deleted', false)->find($orderVideoDetail->product_details_videos_id);
-            if ($orderVideoDetail->orderDetail->order->status == 'ok' && $orderVideoDetail->orderDetail->order->users_id == $user_id && !$orderVideoDetail->orderDetail->all_videos_buy && $orderVideoDetail->orderDetail->product->type == 'video') {
-                $found_user_videoSession = UserVideoSession::where('users_id', $user_id)->where('video_sessions_id', $product_detail_video->video_sessions_id)->first();
-                $price = $product_detail_video->price != null ? $product_detail_video->price : $product_detail_video->videoSession->price;
-                $checkPriceAndUserVideoSession = (!$price || $found_user_videoSession);
-                $orderVideoDetailResource = (new OrderVideoDetailResource($orderVideoDetail))->check($checkPriceAndUserVideoSession);
-                $orderVideoDetailsArr[] = $orderVideoDetailResource;
-            }
+            $found_user_videoSession = UserVideoSession::where('users_id', $user_id)->where('video_sessions_id', $product_detail_video->video_sessions_id)->first();
+            $price = $product_detail_video->price != null ? $product_detail_video->price : $product_detail_video->videoSession->price;
+            $checkPriceAndUserVideoSession = (!$price || $found_user_videoSession);
+            $orderVideoDetailResource = (new OrderVideoDetailResource($orderVideoDetail))->check($checkPriceAndUserVideoSession);
+            $orderVideoDetailsArr[] = $orderVideoDetailResource;
         }
         return ((new OrderVideoDetailCollection($orderVideoDetailsArr)))->additional([
             'error' => null,
         ])->response()->setStatusCode(200);
-
     }
 }
