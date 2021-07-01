a49bb6e0b10f0b743c9354a996f6e6a4e7dfd7b4
marykhodaparast <maryam.kh1374@gmail.com>
2021-07-01 17:15:44 +0430

    first_name is not required

--- app/Http/Requests/UserCreateRequest.php
+++ app/Http/Requests/UserCreateRequest.php
@@ -27,7 +27,7 @@ class UserCreateRequest extends FormRequest
     public function rules()
     {
         return [
-            'first_name' => 'required|string|between:2,100',
+            'first_name' => 'nullable|string|between:2,100',
             'last_name' => 'required|string|between:2,100',
             'email' => 'required|string|max:12|unique:users',
             'password' => 'required|string|confirmed|min:6',
