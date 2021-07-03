7136b18c3b99da6fcef5f8f7bc855ad62a2d9d96
marykhodaparast <maryam.kh1374@gmail.com>
2021-07-03 14:03:10 +0430

    change productscontroller  in admin but i should ask mr shakeri to give me necessary parameters that he needs

--- app/Http/Resources/ProductVideoResource.php
+++ app/Http/Resources/ProductVideoResource.php
@@ -24,25 +24,26 @@ class ProductVideoResource extends JsonResource
     {
 
         if ($this->resource != null) {
-            $number = new Number2Word;
-            $num = 0;
-            if($this->videoSession){
-                $numToWord = $this->foo[$this->id] ? $number->numberToWords($this->foo[$this->id]) : $this->product_detail_video_name;
-                if (!$this->foo[$this->id]) {
-                    $num = $numToWord;
-                } else {
-                    $num = strpos($numToWord, "سه") !== false ? str_replace("سه", "سو", $numToWord) . 'م' : $numToWord . 'م';
-                }
-            }
+            // $number = new Number2Word;
+            // $num = 0;
+            // if($this->videoSession){
+            //     $numToWord = $this->foo[$this->id] ? $number->numberToWords($this->foo[$this->id]) : $this->product_detail_video_name;
+            //     if (!$this->foo[$this->id]) {
+            //         $num = $numToWord;
+            //     } else {
+            //         $num = strpos($numToWord, "سه") !== false ? str_replace("سه", "سو", $numToWord) . 'م' : $numToWord . 'م';
+            //     }
+            // }
             return [
                 'id' => $this->id,
                 'start_date' => $this->videoSession ? $this->videoSession->start_date : null,
                 'start_time' => $this->videoSession ? date('H:i', strtotime($this->videoSession->start_time)) : null,
                 'end_time' => $this->videoSession ? date('H:i', strtotime($this->videoSession->end_time)) : null,
                 'teacher'  => $this->videoSession ? new UserResource($this->videoSession->teacher) : null,
-                'name' => $this->name == null ? $num : $this->name,
+                //'name' => $this->name == null ? $num : $this->name,
+                'name' => $this->name, 
                 'price' => $this->price == null ? ($this->videoSession ? $this->videoSession->price : null) : $this->price,
-                'product' => new ProductResource($this->product),
+                //'product' => new ProductResource($this->product),
                 'video_session_type' => $this->videoSession ? $this->videoSession->video_session_type : null,
                 'video_link' => $this->videoSession ? $this->videoSession->video_link : null,
                 'extraordinary' => $this->extraordinary,
