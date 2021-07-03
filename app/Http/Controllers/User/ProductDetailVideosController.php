80e12991393dd500ff42b0ef7941094edd21f1e6
marykhodaparast <maryam.kh1374@gmail.com>
2021-07-03 12:44:10 +0430

    remove comments

--- app/Http/Controllers/User/ProductDetailVideosController.php
+++ app/Http/Controllers/User/ProductDetailVideosController.php
@@ -21,26 +21,10 @@ class ProductDetailVideosController extends Controller
     public function show($id)
     {
 
-        //$number = new Number2Word;
         $getNameOfSessions = new GetNameOfSessions;
         $product_detail_video = ProductDetailVideo::where('is_deleted', false)->find($id);
         $product_detail_videos = [];
         if ($product_detail_video != null) {
-            // $product = $product_detail_video->product;
-            // $numArray = [];
-            // $i = 1;
-            // for ($indx = 0; $indx < count($product->productDetailVideos); $indx++) {
-            //     $v = $product->productDetailVideos[$indx];
-            //     $numArray[$v->id] = $v != null && $product->productDetailVideos[$indx]->extraordinary ? 0 : $i;
-            //     $i = $numArray[$v->id] ? $i + 1 : $i;
-            //     $product_detail_videos[] = $product->productDetailVideos[$indx];
-            // }
-            // for($j = 0; $j < count($product_detail_videos); $j++) {
-            //     $persianAlphabetNum = $number->numberToWords($numArray[$product_detail_videos[$j]->id]);
-            //     if($persianAlphabetNum != null) {
-            //         $product_detail_videos[$j]->name = $product_detail_videos[$j]->name == null ? (strpos($persianAlphabetNum, "سه") !== false ? str_replace("سه", "سو", $persianAlphabetNum) . 'م' : $persianAlphabetNum . 'م') : $product_detail_videos[$j]->name;
-            //     }
-            // }
             $product_detail_videos = $getNameOfSessions->getProductDetailVideos($product_detail_video->product);
             foreach($product_detail_videos as $item) {
                if($item->id == $product_detail_video->id) {
