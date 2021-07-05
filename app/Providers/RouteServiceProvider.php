16c6a2f645ed02f3d963935f21ce66689a24756d
marykhodaparast <maryam.kh1374@gmail.com>
2021-07-05 17:26:53 +0430

    add some routes in servers.php

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -50,6 +50,10 @@ class RouteServiceProvider extends ServiceProvider
             Route::prefix('user')
                 ->namespace($this->namespace)
                 ->group(base_path('routes/user.php'));
+
+            Route::prefix('server')
+                ->namespace($this->namespace)
+                ->group(base_path('routes/server.php'));
         });
     }
 
