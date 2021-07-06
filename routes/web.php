b830a749c3b6265c2e004e1c3f45a750cc9f59af
marykhodaparast <maryam.kh1374@gmail.com>
2021-07-06 18:36:06 +0430

    add a simple example but pusher pusher-php-server is not installed yet because it had problem with composer

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Events\WebsocketDemoEvent;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +15,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    broadcast(new WebsocketDemoEvent('some data'));
     return view('welcome');
 });
