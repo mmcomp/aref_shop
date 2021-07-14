886373fdebadc2141f126708f1b04c076db7d4c4
marykhodaparast <maryam.kh1374@gmail.com>
2021-07-14 13:37:36 +0430

    add "" instead of null

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -20,7 +20,7 @@ class UserResource extends JsonResource
             return [
                 'id' => $this->id,
                 'email' => $this->email,
-                'first_name' => $this->first_name,
+                'first_name' => $this->first_name == null ? "" : $this->first_name,
                 'last_name' => $this->last_name,
                 'avatar_path' => $this->avatar_path,
                 'referrer_user' => new UserResource($this->referrerUser),
