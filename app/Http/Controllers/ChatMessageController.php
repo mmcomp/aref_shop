ff8497cf295f55616c10631513e228a3e0d1c322
marykhodaparast <maryam.kh1374@gmail.com>
2021-07-24 10:15:46 +0430

    add function disable_chats

---
+++ app/Http/Controllers/ChatMessageController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ChatMessageController extends Controller
+{
+    
+    public function disable_chats()
+    {
+        
+    }
+}
