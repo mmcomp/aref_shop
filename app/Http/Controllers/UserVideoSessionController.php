fec19b24f6359804aa76850396f13c841661d1b8
maryam khodaparast <khcomputergirl@gmail.com>
2021-08-09 15:31:15 +0430

    add report for absence presence

---
+++ app/Http/Controllers/UserVideoSessionController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\UserVideoSessionReportRequest;
+use App\Http\Resources\UserVideoSessionCollection;
+use App\Models\UserVideoSession;
+use App\Models\ProductDetailVideo;
+
+class UserVideoSessionController extends Controller
+{
+
+    public function report(UserVideoSessionReportRequest $request)
+    {
+
+        $product_detail_videos_id = $request->input('product_detail_videos_id');
+        $productDetailVideo = ProductDetailVideo::where('is_deleted', false)->find($product_detail_videos_id);
+        $video_sessions_id = $productDetailVideo->video_sessions_id;
+        $user_video_sessions = UserVideoSession::where('video_sessions_id', $video_sessions_id)->get();
+        return (new UserVideoSessionCollection($user_video_sessions))->additional([
+            'errors' => null,
+        ])->response()->setStatusCode(200);
+    }
+}
---
+++ app/Http/Resources/UserVideoSessionResource.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class UserVideoSessionResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        if($this->resource != null) {
+            return [
+               'id' => $this->id,
+               'user' => new UserWithFirstNameLastNameEmailResource($this->user),
+               'offline_spend' => $this->offline_spend,
+               'offline_started_at' => $this->offline_started_at,
+               'offline_exited_at' => $this->offline_exited_at,
+               'online_spend' => $this->online_spend,
+               'online_started_at' => $this->online_started_at,
+               'online_exited_at' => $this->online_exited_at,
+               'created_at' => $this->created_at,
+               'updated_at' => $this->updated_at
+            ];
+        }
+    }
+}
