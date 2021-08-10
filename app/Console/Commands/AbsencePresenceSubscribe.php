8ed5adb8195dd25a030be0fefa26d55bf5a6bc36
Mehrdad Mirsamie <m.mirsamie@gmail.com>
2021-08-10 09:19:37 +0430

    handle is first

--- app/Console/Commands/AbsencePresenceSubscribe.php
+++ app/Console/Commands/AbsencePresenceSubscribe.php
@@ -48,22 +48,25 @@ class AbsencePresenceSubscribe extends Command
             $product_detail_video = ProductDetailVideo::where('is_deleted', false)->find($product_detail_videos_id);
             $users_id = $json_decode_message["users_id"];
             $type = $json_decode_message["type"];
+            $isFirst = isset($json_decode_message["isFirst"]) && $json_decode_message["isFirst"];
             $user_video_session = UserVideoSession::where('video_sessions_id', $product_detail_video->video_sessions_id)->where('users_id', $users_id)->first();
             // Log::info($user_video_session->id);
             if ($type == "online") {
-                if ($user_video_session->online_started_at == null) {
+                if ($user_video_session->online_started_at == null && $isFirst) {
                     $user_video_session->online_started_at = now();
-                } else {
+                    $user_video_session->online_spend += 5;
+                } else if ($user_video_session->online_started_at != null) {
                     $user_video_session->online_exited_at = now();
+                    $user_video_session->online_spend += 5;
                 }
-                $user_video_session->online_spend += 5;
             } else {
-                if ($user_video_session->offline_started_at == null) {
+                if ($user_video_session->offline_started_at == null && $isFirst) {
                     $user_video_session->offline_started_at = now();
+                    $user_video_session->offline_spend += 5;
                 } else {
                     $user_video_session->offline_exited_at = now();
+                    $user_video_session->offline_spend += 5;
                 }
-                $user_video_session->offline_spend += 5;
             }
             $user_video_session->save();
 
