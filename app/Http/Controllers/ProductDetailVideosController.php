9f96931a241c35799f3f7799662cd80e2b3590f1
maryam khodaparast <khcomputergirl@gmail.com>
2021-08-17 09:27:46 +0430

    .

--- app/Http/Controllers/ProductDetailVideosController.php
+++ app/Http/Controllers/ProductDetailVideosController.php
@@ -174,8 +174,8 @@ class ProductDetailVideosController extends Controller
         if ($product_detail_video->videoSession && $lastProductDetailVideoOfTheRequestedProduct) {
             $raiseError->ValidationError(($lastProductDetailVideoOfTheRequestedProduct->start_date > $product_detail_video->videoSession->start_date && !$request->input('extraordinary')), ['extraordinary' => ['The extraordinary field should be 1']]);
         }
-        $found_product_detail_video = ProductDetailVideo::where('is_deleted', false)->where('products_id', $request->input('products_id'))->where('video_sessions_id', $product_detail_video->video_sessions_id)->first();
-        $raiseError->validationError($found_product_detail_video, ['product_detail_video' => ['The product_detail_video is already recorded!']]);
+        //$found_product_detail_video = ProductDetailVideo::where('is_deleted', false)->where('products_id', $request->input('products_id'))->where('video_sessions_id', $product_detail_video->video_sessions_id)->first();
+        //$raiseError->validationError($found_product_detail_video, ['product_detail_video' => ['The product_detail_video is already recorded!']]);
         ProductDetailVideo::create([
             'products_id' => $request->input('products_id'),
             'name' => $request->input('name'),
