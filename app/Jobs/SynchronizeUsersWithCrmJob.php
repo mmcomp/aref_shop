a7dabf1db6118722ab3ddda56bbf38f9880659f6
Hscomp <hscomp2002@gmail.com>
2021-10-30 16:38:28 +0330

    Sync Users

--- app/Jobs/SynchronizeUsersWithCrmJob.php
+++ app/Jobs/SynchronizeUsersWithCrmJob.php
@@ -29,52 +29,57 @@ class SynchronizeUsersWithCrmJob implements ShouldQueue
     }
 
     /**
+     * Execute cUrl
+     *
+     * @return object
+     */
+    public function getCrmData($params)
+    {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, env('CRM_ADD_STUDENT_URL'));
+        $data_string = json_encode($params, JSON_UNESCAPED_UNICODE);
+        //curl_setopt($ch, CURLOPT_URL,"http://192.168.1.81/test.php");
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, [
+            'Content-Type: application/json',
+            'Content-Length: ' . strlen($data_string)
+        ]);
+
+        $result = curl_exec($ch);
+        curl_close($ch);
+        return $result;
+    }
+
+    /**
      * Execute the job.
      *
-     * @return void
+     * @return object
      */
     public function handle()
     {
         Log::info("CRM_ADD_STUDENT");
         try {
-            Log::info("CRM_ADD_STUDENT_Try" . json_encode($this->user));
-            // $response = Http::post(env('CRM_ADD_STUDENT_URL'), [
-            //     "students" => [
-            //         0 => [
-            //             "phone" => $this->user->email,
-            //             "last_name" => $this->user->last_name,
-            //             'introducing' => 'عضویت در سایت'
-            //         ],
-            //     ],
-            // ]);
-
-            $response = Http::withoutVerifying()
-                ->withOptions(["verify" => false])->post("http://crm.aref-group.ir/api/students", [
-                    "students" => [
-                        0 => [
-                            "phone" => $this->user->email,
-                            "last_name" => $this->user->last_name,
-                            'introducing' => 'عضویت در سایت'
-                        ],
+            $response = $this->getCrmData( [
+                "students" => [
+                    0 => [
+                        "phone" => $this->user->email,
+                        "last_name" => $this->user->last_name,
+                        'introducing' => 'عضویت در سایت'
                     ],
-                ]);
+                ],
+            ]);
 
-            Log::info("CRM_ADD_STUDENT_SUCCESS");
-            // if ($response->getStatusCode() == 200) {
-            //     Log::info("CRM_ADD_STUDENT_SUCCESS_200");
-            //     UserSync::create([
-            //         "users_id" => $this->user->id,
-            //         "status" => "successful",
-            //         "error_message" => null,
-            //     ]);
-            // } else {
-            //     Log::info("CRM_ADD_STUDENT_SUCCESS:" . $response->getStatusCode());
-            //     UserSync::create([
-            //         "users_id" => $this->user->id,
-            //         "status" => "failed",
-            //         "error_message" => "the response was not successful" . $response->getStatusCode(),
-            //     ]);
-            // }
+            if ( count($response->added_ids)>0) {
+                Log::info("CRM_ADD_STUDENT_SUCCESS_200");
+                UserSync::create([
+                    "users_id" => $this->user->id,
+                    "status" => "successful",
+                    "error_message" => null,
+                ]);
+            } 
         } catch (Throwable $e) {
             Log::info("CRM ran into a problem in synchronize users!" . json_encode($e->getMessage()));
             UserSync::create([
