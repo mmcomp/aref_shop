f6e96ea36ff111aabd2b1386d629132af3f26d52
majid nazari <majidnazarister@gmail.com>
2021-12-07 09:29:54 +0330

    edit group by confrence users query

--- app/Http/Controllers/ConferenceUsersController.php
+++ app/Http/Controllers/ConferenceUsersController.php
@@ -16,9 +16,9 @@ class ConferenceUsersController extends Controller
 {
     public function show($product_detail_videos_id)
     {       
-       $conference=DB::select("SELECT DISTINCT email,first_name,last_name , referrer , product_detail_videos_id ,name , already_registerd FROM conference_users left join users on (users.id = users_id) 
+       $conference=DB::select("SELECT  email,first_name,last_name , referrer , product_detail_videos_id , name  FROM conference_users left join users on (users.id = users_id) 
        left join product_detail_videos on (product_detail_videos.id=product_detail_videos_id)
-       WHERE product_detail_videos_id=$product_detail_videos_id  ");
+       WHERE product_detail_videos.id=418 group by users.email");
        
        //$result=new Paginator($conference, 50);
        //return response()->json($conference,200);
