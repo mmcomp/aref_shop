b27863a0c0f236706a764cbc5523e7f7f768198d
majid nazari <majidnazarister@gmail.com>
2022-01-16 14:52:39 +0330

    add  created_at to team get all

--- app/Http/Resources/ShowAllTeamResource.php
+++ app/Http/Resources/ShowAllTeamResource.php
@@ -14,12 +14,13 @@ class ShowAllTeamResource extends JsonResource
      * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
      */
     public function toArray($request)
-    {       
+    {              
         return [
             "id" => $this->id,
             "user_id_creator" =>$this->user_id_creator,
             "name" =>$this->name,
             "is_full" => $this->is_full,
+            "created_at" => $this->created_at,
             // "team_member" =>(new TeamMemberCollection($this->team_member)),
             "team_member" =>(new TeamMemberCollection($this->TeamMember)),
             "leader" => $this->leader
