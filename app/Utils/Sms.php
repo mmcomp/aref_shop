9565feabe19730a63c03b89fc87ffdd4c2b032a7
Setavin <setavin@server.com>
2022-08-22 20:19:34 +0430

    .

--- app/Utils/Sms.php
+++ app/Utils/Sms.php
@@ -3,12 +3,12 @@ namespace App\Utils;
 
 class Sms {
     public function __construct() {
-        $api_key = "553133726A6962423652346246504B544C72766668784A6E384C61682B4D565349756B2B374D374C4A6D553D";
+        $api_key = "484A30787835725542714A394B6979586242372F654D454E4B524F39676D31385673684C52724C344C5A513D";
         $this->url = "https://api.kavenegar.com/v1/$api_key/verify/lookup.json";
         $this->smsUrl="https://api.kavenegar.com/v1/$api_key/sms/send.json";
     }
 
-    public function sendCode($receptor, $token, $template='aref') {
+    public function sendCode($receptor, $token, $template='setavin') {
         $ch = curl_init();
         $url = $this->url."?receptor=$receptor&token=$token&template=$template";
         curl_setopt($ch, CURLOPT_URL,$url);
