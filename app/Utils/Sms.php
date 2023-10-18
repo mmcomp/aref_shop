16af975d5b7f7f1262f10aea19a61572769c570b
hscomp2002 <hscomp2002@gmail.com>
2023-10-18 08:51:52 +0330

    change sms api from hscomp2002 to banuee

--- app/Utils/Sms.php
+++ app/Utils/Sms.php
@@ -3,7 +3,8 @@ namespace App\Utils;
 
 class Sms {
     public function __construct() {
-        $api_key = "484A30787835725542714A394B6979586242372F654D454E4B524F39676D31385673684C52724C344C5A513D";
+        //$api_key = "484A30787835725542714A394B6979586242372F654D454E4B524F39676D31385673684C52724C344C5A513D";
+        $api_key = "553133726A6962423652346246504B544C72766668784A6E384C61682B4D565349756B2B374D374C4A6D553D";
         $this->url = "https://api.kavenegar.com/v1/$api_key/verify/lookup.json";
         $this->smsUrl="https://api.kavenegar.com/v1/$api_key/sms/send.json";
     }
