c5f74c6d244a19a4a6370e3ef2f62e6f44b90a11
Mehrdad Mirsamie <m.mirsamie@gmail.com>
2023-10-29 08:30:41 +0000

    Finish

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -3,33 +3,14 @@
 namespace Tests\Unit;
 
 use PHPUnit\Framework\TestCase;
-use Carbon\Carbon;
 
 class ExampleTest extends TestCase
 {
     /**
      * A basic test example.
-     *
-     * @return void
      */
-    public function testBasicTest()
+    public function test_that_true_is_true(): void
     {
-        $this->assertTrue(true);
-    }
-
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function testBasicTest3()
-    {
-        $user = [
-            "email" => "0902888145"
-        ];
-        // $test = resolve(\App\Jobs\SynchronizeUsersWithCrmJob::class,['user'=>$user]);
-        // $test->dispatch($user)->delay(Carbon::now()->addSecond(env('CRM_ADD_STUDENT_TIMEOUT')));
-
         $this->assertTrue(true);
     }
 }
