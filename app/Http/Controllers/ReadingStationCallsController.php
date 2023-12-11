b563efc7a4717b5830c3d0c4f5b87db6d0e6e24c
Mehrdad Mirsamie <m.mirsamie@gmail.com>
2023-12-11 09:23:34 +0000

    .

--- app/Http/Controllers/ReadingStationCallsController.php
+++ app/Http/Controllers/ReadingStationCallsController.php
@@ -81,7 +81,7 @@ class ReadingStationCallsController extends Controller
             ])->response()->setStatusCode(400);
         }
 
-        $now = Carbon::now()->toDateString();
+        $now = Carbon::now()->toDayDateTimeString();
         $reasons = [];
         if ($slutUser->status === 'absent') {
             $reasons[] = "absence";
