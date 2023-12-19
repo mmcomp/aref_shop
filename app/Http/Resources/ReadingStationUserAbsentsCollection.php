88d4d815dc74e1995ba6b768935e769ff6697544
Mehrdad Mirsamie <m.mirsamie@gmail.com>
2023-12-19 10:47:26 +0000

    .

--- app/Http/Resources/ReadingStationUserAbsentsCollection.php
+++ app/Http/Resources/ReadingStationUserAbsentsCollection.php
@@ -15,15 +15,12 @@ class ReadingStationUserAbsentsCollection extends ResourceCollection
      */
     public function toArray($request)
     {
-        // dump($this->colllection);
         $groupId = 0;
         foreach ($this->collection as $indx => $cell) {
             if ($indx === 0) continue;
 
             $a = $this->collection[$indx - 1];
             $b = $cell;
-            // dump($a);
-            // dump($b);
             if ($this->near($a, $b)) {
                 if ($a->groupId) {
                     $this->collection[$indx]->groupId = $a->groupId;
@@ -45,57 +42,9 @@ class ReadingStationUserAbsentsCollection extends ResourceCollection
             }
         }
         return $this->collection->groupBy('groupId');
-        /*
-        $result = [];
-        foreach ($this->collection as $indx => $cell) {
-            if ($indx === 0) {
-                $result [] = $cell;
-                continue;
-            }
-
-            $lastArray = $result[count($result) - 1];
-            $lastCell = null;
-            if (!is_array($lastArray)) {
-                $lastArray = null;
-                $lastCell = $result[count($result) - 1];
-            } else {
-                $lastCell = $lastArray[count($lastArray) - 1];
-            }
-            // dump($lastArray);
-            // dump($lastCell);
-            $a = $this->collection[$indx - 1];
-            $b = $this->collection[$indx];
-            if ($this->near($a, $b)) {
-                if ($this->near($lastCell, $a)) {
-                    if ($lastArray) {
-                        $result[count($result) - 1][] = $a;
-                        $result[count($result) - 1][] = $b;
-                    } else {
-                        array_pop($result);
-                        $result[] = [$lastCell, $a, $b];
-                    }
-                } else {
-                    $result[] = [$a, $b];
-                }
-            } else {
-                if ($this->near($lastCell, $a)) {
-                    if ($lastArray) {
-                        $result[count($result) - 1][] = $a;
-                    } else {
-                        array_pop($result);
-                        $result[] = [$lastCell, $a];
-                    }
-                } else {
-                    $result[] = [$a, $b];
-                }
-            }
-        }
-        // dd();
-        return $result;
-        */
     }
 
-    private function near($a, $b, $maxDiffMinutes = 15): bool
+    private function near($a, $b, $maxDiffMinutes = 45): bool
     {
         return (Carbon::parse($b->slut->start)->diffInMinutes(Carbon::parse($a->slut->end)) <= $maxDiffMinutes);
     }
