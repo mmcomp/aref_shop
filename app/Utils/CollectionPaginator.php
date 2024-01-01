fbf29ea02a39c5cf4cb114dabf369aca5e64e9f1
Mehrdad Mirsamie <m.mirsamie@gmail.com>
2024-01-01 10:19:57 +0000

    .

--- app/Utils/CollectionPaginator.php
+++ app/Utils/CollectionPaginator.php
@@ -2,7 +2,20 @@
 namespace App\Utils;
 
 use Illuminate\Pagination\LengthAwarePaginator;
+use stdClass;
 
 class CollectionPaginator extends LengthAwarePaginator {
-    
+    public $meta;
+
+    function __construct($items, $total, $perPage, $currentPage = null, array $options = [])
+    {
+        parent::__construct($items, $total, $perPage, $currentPage, $options);
+    }
+
+    public function toArray()
+    {
+        $res = parent::toArray();
+        $res['meta'] = ['last_page' => $res['last_page']];
+        return $res;
+    }
 }
