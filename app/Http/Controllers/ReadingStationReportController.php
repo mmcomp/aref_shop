030da504fb6252e127898c07685a3ef16490ab0f
Mehrdad Mirsamie <m.mirsamie@gmail.com>
2024-01-03 07:39:12 +0000

    .

--- app/Http/Controllers/ReadingStationReportController.php
+++ app/Http/Controllers/ReadingStationReportController.php
@@ -4,14 +4,29 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\ReadingStationEducationalReportRequest;
 use App\Http\Resources\ReadingStationEducationalReportCollection;
+use App\Http\Resources\ReadingStationEducationalReportResource;
 use App\Models\ReadingStation;
 use App\Models\ReadingStationSlutUser;
+use Carbon\Carbon;
 use Illuminate\Support\Facades\DB;
 
 class ReadingStationReportController extends Controller
 {
     public function educational(ReadingStationEducationalReportRequest $request, ReadingStation $readingStation)
     {
+        $from = Carbon::parse($request->from_date);
+        $to = Carbon::parse($request->to_date);
+        if ($from->greaterThan($to)) {
+            return (new ReadingStationEducationalReportResource(null))->additional([
+                'errors' => ['reading_station_report' => ['from date should be before to date!']],
+            ])->response()->setStatusCode(400);
+        }
+        $diff = intval(env('EDUCATIONAL_REPORT_DATE_DIFF')) ?? 7;
+        if($to->diffInDays($from) > $diff) {
+            return (new ReadingStationEducationalReportResource(null))->additional([
+                'errors' => ['reading_station_report' => ['report time difference is higher than it should be!', $diff]],
+            ])->response()->setStatusCode(400);
+        }
         $slutUsers = ReadingStationSlutUser::whereHas('weeklyProgram', function ($q1) use ($readingStation, $request) {
             $q1->whereHas('readingStationUser', function ($q2) use ($readingStation, $request) {
                 $q2->where('reading_station_id', $readingStation->id);
