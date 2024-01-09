78c6b48c14256e339d714ce75ed19065b4ea2a33
Mehrdad Mirsamie <m.mirsamie@gmail.com>
2024-01-09 08:04:12 +0000

    .

--- app/Http/Controllers/ReadingStationSlutUsersController.php
+++ app/Http/Controllers/ReadingStationSlutUsersController.php
@@ -174,7 +174,7 @@ class ReadingStationSlutUsersController extends Controller
         $nextWeekEnd = $nextWeekEndDate->toDateString();
         $package = ReadingStationPackage::find($request->next_week_package_id);
         $weeklyPrograms = $user->readingStationUser->weeklyPrograms;
-        dd($weeklyPrograms);
+        dd($user->readingStationUser);
         foreach ($weeklyPrograms as $weeklyProgram) {
             if (Carbon::parse($weeklyProgram->start)->diffInDays($nextWeekStartDate) === 0) {
                 dump('found');
