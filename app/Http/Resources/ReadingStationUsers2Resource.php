eeb9e7954bb69a102ea4cebab1bc448c98a4fd90
Mehrdad Mirsamie <m.mirsamie@gmail.com>
2024-04-13 12:09:07 +0000

    .

--- app/Http/Resources/ReadingStationUsers2Resource.php
+++ app/Http/Resources/ReadingStationUsers2Resource.php
@@ -21,8 +21,8 @@ class ReadingStationUsers2Resource extends JsonResource
             return [
                 'id' => $this->id,
                 'table_number' => $this->table_number,
-                'user'=> new UserResource($this->user),
-                'package' => new ReadingStationPackagesResource($this->package),
+                // 'user'=> new UserResource($this->user),
+                // 'package' => new ReadingStationPackagesResource($this->package),
                 'total' => $this->total,
                 // 'weeklyPrograms' => new ReadingStationWeeklyPrograms2Collection($this->weeklyPrograms),
             ];
