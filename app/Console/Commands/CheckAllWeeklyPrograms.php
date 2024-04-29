5d89b821234278dd17cbb44c7ff9eb888e444b1f
Mehrdad Mirsamie <m.mirsamie@gmail.com>
2024-04-29 12:41:51 +0000

    .

--- app/Console/Commands/CheckAllWeeklyPrograms.php
+++ app/Console/Commands/CheckAllWeeklyPrograms.php
@@ -29,29 +29,33 @@ class CheckAllWeeklyPrograms extends Command
     {
         $weeklyPrograms = ReadingStationWeeklyProgram::all();
         foreach ($weeklyPrograms as $weeklyProgram) {
+            $readingStationUser = $weeklyProgram->readingStationUser;
+            if ($readingStationUser->id !== 11) {
+                continue;
+            }
             $score = 0;
             $diff = $weeklyProgram->required_time_done + $weeklyProgram->optional_time_done - $weeklyProgram->required_time - $weeklyProgram->optional_time;
             if (!$weeklyProgram->readingStationUser) continue;
             $package = $weeklyProgram->readingStationUser->package;
             $user = $weeklyProgram->readingStationUser->user;
-            $readingStationUser = $weeklyProgram->readingStationUser;
-            if ($readingStationUser->last_weekly_program === $weeklyProgram->id) {
-                continue;
+            if ($readingStationUser->last_weekly_program !== $weeklyProgram->id) {
+                // package diff done score
+                if ($diff < 0) {
+                    $score = -2;
+                } elseif ($diff > 0) {
+                    $step = ($package->step ?? 10) * 60;
+                    $score = ($diff - ($diff % $step)) * 2 / $step;
+                }
             }
+            echo "diff = $diff score = $score\n";
 
-            // package diff done score
-            if ($diff < 0) {
-                $score = -2;
-            } elseif ($diff > 0) {
-                $step = ($package->step ?? 10) * 60;
-                $score = ($diff - ($diff % $step)) * 2 / $step;
-            }
 
             // no absent score
             if ($weeklyProgram->absent_day === 0 && $weeklyProgram->late_day === 0) {
                 $score += 3;
                 $weeklyProgram->being_point += 3;
             }
+            echo "no absent score = $score\n";
 
             // package grade score
             if ($package->grade && $user->grade) {
@@ -60,6 +64,7 @@ class CheckAllWeeklyPrograms extends Command
                     $weeklyProgram->being_point += ($package->grade - $user->grade) * 3;
                 }
             }
+            echo "package grade score = $score\n";
 
             $weeklyProgram->point += $score;
             $weeklyProgram->save();
