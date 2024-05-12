e7a7e31c1f5efe4b5d6dd7cbe4ba010396ebeb61
Mehrdad Mirsamie <m.mirsamie@gmail.com>
2024-05-12 08:35:25 +0000

    .

--- app/Console/Commands/CheckAllWeeklyPrograms.php
+++ app/Console/Commands/CheckAllWeeklyPrograms.php
@@ -77,7 +77,7 @@ class CheckAllWeeklyPrograms extends Command
             if (count($weeklyProgram->sluts) === 0) continue;
             if (!$weeklyProgram->sluts->where('status', '!=', 'defined')->where('deleted_at', null)->first()) continue;
             $readingStationUser = $weeklyProgram->readingStationUser;
-            if ($readingStationUser->id !== 16) {
+            if ($readingStationUser->id !== 32) {
                 continue;
             }
             echo "Week[$readingStationUser->id $weeklyProgram->id] : $weeklyProgram->start - $weeklyProgram->end\n";
