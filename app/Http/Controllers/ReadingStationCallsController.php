1e4d6d0a853212581ecca670bd45ac4b416fdbf2
Mehrdad Mirsamie <m.mirsamie@gmail.com>
2024-06-06 09:53:04 +0000

    .

--- app/Http/Controllers/ReadingStationCallsController.php
+++ app/Http/Controllers/ReadingStationCallsController.php
@@ -84,7 +84,6 @@ class ReadingStationCallsController extends Controller
             ->withAggregate('slut', 'start')
             ->orderBy('slut_start')
             ->get();
-        dd($todaySluts);
         // foreach($todaySluts as $indx => $todaySlut) {
         //     $absentPresent = $absentPresents->where('user_id', $todaySlut->weeklyProgram->readingStationUser->user->id)->first();
         //     $todaySluts[$indx]->absentPresent = $absentPresent;
